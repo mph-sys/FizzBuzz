d65b8b2623d530cbc5ee92453fb1e81f4db1abc9
mph-sys <phung.mathieu@gmail.com>
2026-02-18 16:02:40 +0100

    fix fizzbuzz logic

--- pkg/fizzbuzz.go
+++ pkg/fizzbuzz.go
@@ -3,7 +3,6 @@ package pkg
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"strconv"
 	"test-lbc/pkg/models"
 )
@@ -18,59 +17,61 @@ func NewFizzBuzzService(db *sql.DB) FizzBuzzService {
 	}
 }
 
-func (s FizzBuzzService) Run(params models.FizzBuzzParams) []string {
+func (s FizzBuzzService) Run(params models.FizzBuzzParams) ([]string, error) {
 	var (
 		result       = make([]string, params.Limit)
 		currentValue = 1
 	)
+
 	if params.Int1 == 0 && params.Int2 == 0 {
 		for i := range result {
 			result[i] = strconv.Itoa(currentValue)
 			currentValue++
 		}
-		return result
+		return result, nil
 	}
-	if params.Int1 == 0 {
-		params.Int1 = params.Limit + 1
+
+	int1TestValue, int2TestValue := params.Int1, params.Int2
+	if int1TestValue == 0 {
+		int1TestValue = params.Limit + 1
 	}
-	if params.Int2 == 0 {
-		params.Int2 = params.Limit + 1
+	if int2TestValue == 0 {
+		int2TestValue = params.Limit + 1
 	}
 
 	for i := range result {
 		switch {
-		case currentValue%(params.Int1*params.Int2) == 0:
+		case currentValue%(int1TestValue*int2TestValue) == 0:
 			result[i] = params.Str1 + params.Str2
-		case currentValue%params.Int1 == 0:
+		case currentValue%int1TestValue == 0:
 			result[i] = params.Str1
-		case currentValue%params.Int2 == 0:
+		case currentValue%int2TestValue == 0:
 			result[i] = params.Str2
 		default:
 			result[i] = strconv.Itoa(currentValue)
 		}
 		currentValue++
 	}
-	err := s.IncStats(params)
-	if err != nil {
-		log.Println(err)
-	}
-	return result
+
+	return result, s.incStats(params)
 }
 
-func (s *FizzBuzzService) IncStats(params models.FizzBuzzParams) error {
+func (s *FizzBuzzService) incStats(params models.FizzBuzzParams) error {
 	_, err := s.db.Exec("INSERT INTO `stats` (`int1`,`int2`,`limit`,`str1`,`str2`,`hits`) VALUES (?,?,?,?,?,?) ON DUPLICATE KEY UPDATE `hits` = `hits`+1", params.Int1, params.Int2, params.Limit, params.Str1, params.Str2, 1)
 	if err != nil {
-		return fmt.Errorf("failed to save request: %s", err.Error())
+		return fmt.Errorf("failed to save request: %v", err)
 	}
+
 	return nil
 }
 
 func (s FizzBuzzService) GetMostRequested() (*models.FizzBuzzStats, error) {
 	rows, err := s.db.Query("SELECT `int1`,`int2`,`limit`,`str1`,`str2`,`hits` FROM `stats` ORDER BY `hits` desc LIMIT 1")
 	if err != nil {
-		return nil, fmt.Errorf("failed to query most requested: %s", err.Error())
+		return nil, fmt.Errorf("failed to query most requested: %v", err)
 	}
 	defer rows.Close()
+
 	var mostRequested *models.FizzBuzzStats
 	for rows.Next() {
 		var (
@@ -78,7 +79,7 @@ func (s FizzBuzzService) GetMostRequested() (*models.FizzBuzzStats, error) {
 			str1, str2              string
 		)
 		if err := rows.Scan(&int1, &int2, &limit, &str1, &str2, &hits); err != nil {
-			return nil, fmt.Errorf("failed to scan most requested: %s", err.Error())
+			return nil, fmt.Errorf("failed to scan most requested: %v", err)
 		}
 		mostRequested = &models.FizzBuzzStats{
 			Int1:  int1,
